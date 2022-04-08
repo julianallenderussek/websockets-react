b26eb88844461e07a438c9e39e3c686ded4a491a
julianallenderussek <julianallenderussek@gmail.com>
2022-04-07 19:58:05 -0500

    Open branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import actionCable from 'actioncable'
+
 
 function App() {
+  
+  const [inputText, setInputText ] = useState("")
+  const CableApp = {}
+  let consumer = actionCable.createConsumer('ws://localhost:3000/cable')
+  
+
+  function createSubscription() {
+    register("ConversationsChannel")
+    //register("TsChannel")
+  }
+
+  consumer.onMessage = function (event) {
+    console.log(event.data);
+  }
+
+  const register = async (channel) => {
+    console.log(channel)
+    
+    let response = consumer.subscriptions.create({
+      channel: channel,
+      conversation_id: "555"
+    });
+    console.log(response)
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        This is react
+      </h1>
+      
+      <input 
+        onChange={(e)=>{
+          console.log(e.target.value)
+          setInputText(e.target.value)
+        }}
+      />
+        
+      
+      <button onClick={createSubscription}>
+        Create WebSocket
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import actionCable from 'actioncable'
 
 ReactDOM.render(
   <React.StrictMode>
